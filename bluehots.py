d4e8587c74295858e40d4941f58826152d673d0a
rmdaggett <rmdaggett@dramafever.com>
2018-01-13 10:28:09 -0500

    add support for blizzard blog posts

--- bluehots.py
+++ bluehots.py
@@ -8,7 +8,8 @@ from time import sleep
 
 logging.basicConfig(level=logging.DEBUG)
 
-SOURCE = 'http://www.overstalk.io/heroes/?sources=BLIZZARD_FORUM&_sources=on&_sources=on&_sources=on&_sources=on'
+FORUM_SOURCE = 'http://www.overstalk.io/heroes/?sources=BLIZZARD_FORUM&_sources=on&_sources=on&_sources=on&_sources=on'
+BLOG_SOURCE = 'http://us.battle.net/heroes/en/blog/'
 # TEST_HOOK_URL = 'https://discordapp.com/api/webhooks/395942721405059092/ywqzyE_SspBTlbSbDuiqRuj7Y1GhB5jZA-IrmBoEPWF2hTTBdNxaHBlUioCm-Lxn01U4'
 HOOK_URL = 'https://discordapp.com/api/webhooks/395613235958513664/kp8uEeHVtFaPl2UrjxNopUNSAmmBsb4UWG4rkgmQ3VrZXCgGSQgxNn36TJKB--nBCrpC'
 FIREBASE_URL = 'https://bluehots-d71b6.firebaseio.com/'
@@ -17,13 +18,14 @@ class BlueHots(object):
 
     page = None
     tree = None
-    posts = []
+    forum_posts = []
+    blog_posts = []
     post_dict = defaultdict(dict)
     firebase_app = None
     firebase_db = None
 
     def __init__(self):
-        self.crawl_data(SOURCE)
+        self.crawl_data()
         self.init_firebase()
         self.sync_posts()
 
@@ -45,53 +47,81 @@ class BlueHots(object):
             self.firebase_db.child('posts').child(slug).set(post)
         return self.firebase_db
 
-    def crawl_data(self, source):
-        self.get_page(source)
-        self.get_tree()
-        self.get_posts()
-        self.populate_posts(self.posts)
-        return dict(page=self.page, tree=self.tree, posts=self.posts)
+    def crawl_data(self):
+        forum_page = self.get_page(FORUM_SOURCE)
+        forum_tree = self.get_tree(forum_page)
+        self.get_posts(forum_tree, 'forum')
+
+        blog_page = self.get_page(BLOG_SOURCE)
+        blog_tree = self.get_tree(blog_page)
+        self.get_posts(blog_tree, 'blog')
+
+        self.populate_posts(self.forum_posts, 'forum')
+        self.populate_posts(self.blog_posts, 'blog')
+        return dict(forum_posts=self.forum_posts, blog_posts=self.blog_posts)
 
     def get_page(self, url):
-        self.page = requests.get(url)
-        return self.page
+        return requests.get(url)
 
-    def get_tree(self):
-        self.tree = html.fromstring(self.page.content)
-        return self.tree
+    def get_tree(self, page):
+        return html.fromstring(page.content)
 
-    def get_posts(self):
-        self.posts = self.tree.cssselect('div.card')
-        return self.posts
+    def get_posts(self, tree, post_type):
+        if post_type == 'forum':
+            self.forum_posts = tree.cssselect('div.card')
+        elif post_type == 'blog':
+            self.blog_posts = tree.cssselect('div.container.news-index-section > ul.news-list > li.news-list__item')
 
     @staticmethod
     def clean_string(string):
         return ' '.join(string.replace('\r\n', '').split())
 
-    def get_post_title(self, post):
-        css = 'div.os-post-header'
+    def get_post_title(self, post, post_type):
+        css = None
+        if post_type == 'forum':
+            css = 'div.os-post-header'
+        elif post_type == 'blog':
+            css = 'h2.news-list__item__title'
         title_elm = post.cssselect(css)[0]
         return self.clean_string(title_elm.text_content())
 
-    def get_post_timestamp(self, post):
-        css = 'div.os-post-meta > a:nth-child(3)'
+    def get_post_timestamp(self, post, post_type):
+        css = None
+        if post_type == 'forum':
+            css = 'div.os-post-meta > a:nth-child(3)'
+        elif post_type == 'blog':
+            css = 'span.publish-date'
         timestamp = post.cssselect(css)[0]
         return timestamp.get('title')
 
-    def get_post_body(self, post):
-        css = 'div.os-post-content'
+    def get_post_body(self, post, post_type):
+        css = None
+        if post_type == 'forum':
+            css = 'div.os-post-content'
+        elif post_type == 'blog':
+            css = 'p.news-list__item__description'
         body = post.cssselect(css)[0]
         return self.clean_string(body.text_content())
 
-    def get_post_url(self, post):
-        css = 'div.os-post-header > a'
-        link = post.cssselect(css)[0]
-        return link.get('href')
+    def get_post_url(self, post, post_type):
+        css = None
+        if post_type == 'forum':
+            css = 'div.os-post-header > a'
+            link = post.cssselect(css)[0]
+            return link.get('href')
+        elif post_type == 'blog':
+            css = 'h2.news-list__item__title > a'
+            link = post.cssselect(css)[0]
+            return 'http://us.battle.net' + link.get('href')
 
-    def get_post_slug(self, post):
-        ts = self.get_post_timestamp(post)
-        title = self.get_post_title(post).lower()
-        dt = datetime.strptime(ts, '%d %B %Y %H:%M:%S')
+    def get_post_slug(self, post, post_type):
+        ts = self.get_post_timestamp(post, post_type)
+        title = self.get_post_title(post, post_type).lower()
+        dt = None
+        if post_type == 'forum':
+            dt = datetime.strptime(ts, '%d %B %Y %H:%M:%S')
+        elif post_type == 'blog':
+            dt = datetime.strptime(ts.strip(' PST'), '%b %d, %Y %I:%M %p')
         slug = '{}-{}-{}-{}-{}{}{}'.format(title.split()[0],
                                            title.split()[1],
                                            title.split()[2],
@@ -101,17 +131,18 @@ class BlueHots(object):
                                            dt.year)
         return slug
 
-    def populate_posts(self, posts):
+    def populate_posts(self, posts, post_type):
         for post in posts:
-            ts = self.get_post_timestamp(post)
-            url = self.get_post_url(post)
-            title = self.get_post_title(post)
-            body = self.get_post_body(post)
-            slug = self.get_post_slug(post)
+            ts = self.get_post_timestamp(post, post_type)
+            url = self.get_post_url(post, post_type)
+            title = self.get_post_title(post, post_type)
+            body = self.get_post_body(post, post_type)
+            slug = self.get_post_slug(post, post_type)
             self.post_dict[slug]['url'] = url
             self.post_dict[slug]['title'] = title
             self.post_dict[slug]['body'] = body
             self.post_dict[slug]['date'] = ts
+            self.post_dict[slug]['post_type'] = post_type
         return self.post_dict
 
     def post_to_webhook(self, slug):
